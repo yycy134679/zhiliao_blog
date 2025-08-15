07c526a4d2d79469c925d477342deb5d2a9d7b5b
云烟成雨 <2565858137@qq.com>
2025-08-15 18:25:21 +0800

    5 实现登录和注册布局，并完成模板继承改造

--- zhiliao_blog/urls.py
+++ zhiliao_blog/urls.py
@@ -18,4 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("blog.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path("auth/", include("zlauth.urls")),
+]
